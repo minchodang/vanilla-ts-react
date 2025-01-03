6f0b7010ec2d808b5906935743e8e1d21c7ae088
minchodang <minsu910725@gmail.com>
2025-01-03 15:19:55 +0900

    feat: useEffect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ const HomePage = () => {
     <div>
       <h2>HomePage</h2>
       <a href="/post/1">go post</a>
-      dsfsfsdfds
+      &nbsp;&nbsp;
       <a href="/blog">go blog</a>
     </div>
   );
