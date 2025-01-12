03732c21c5942e5ecb2be8e41176527c7f9f614d
minchodang <minsu910725@gmail.com>
2025-01-12 21:56:16 +0900

    wip: memo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import { useState } from '@/lib/dom/render';
+import BlogPage from './Blog';
+
 const HomePage = () => {
+  const [a, b] = useState(0);
   return (
     <div>
+      <button onclick={() => b((prev) => (prev += 1))}>{a}</button>
       <h2>HomePage</h2>
-      <a href="/blog">go blog</a>
+      {/* <a href="/blog">go blog</a> */}
+      <BlogPage title={''} count={0} />
     </div>
   );
 };
